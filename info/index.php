036b34b34052d8ac7abe018577d182cfc2d20c0d
abu <abumusa.sat@gmail.com>
2024-07-16 16:13:26 +0600

    info file

---
+++ info/index.php
@@ -0,0 +1,78 @@
+
+Metrobank - Multipurpose Personal Portfolio WordPress Theme
+
+https://23july.hostlin.com/metrobank/
+
+Metrobank is a multipurpose portfolio WordPress Theme. This theme is a great solution for all types of professional portfolio websites. The design of the Theme is highly creative and eye-catching. You can use this template to make your portfolio. This template has 7 unique home demos which you can use for making different professional portfolio websites.
+
+This theme is fit for Dentists, Layers, Freelancers, Web developers,UI/UX Designer, Writer, Plumber, and many more professionals.
+
+<ul>
+<li><strong>Elementor Builder: </strong><br />
+<p>Fast, intuitive and smart page Metrobank will make your customization fast and easy. You layout will be ready for publishing in a minute!</p>
+</li>
+</ul>
+<ul>
+<li><strong>Demo content included : </strong><br />Metrobank is ready to use from the box. Quickly install it via FTP or WordPress and after you activate it you can load demo content. Then you can add your own content on already designed pages.</li>
+</ul>
+<ul>
+<li><strong>One click installation : </strong><br />Install Metrobank with our powerful one click installer. Get your site up and running in no time! Quick, easy and rocket fast!</li>
+</ul>
+<ul>
+<li><strong>Responsive &amp; retina ready : </strong><br />Look of your website on mobile devices is very important these days. So we made sure Metrobank looks great both on mobile, desktop and retina screens!</li>
+</ul>
+<ul>
+<li><strong>WPML &amp; Translation Ready: </strong><br />Metrobank is compatible with most popular WordPress plugin that supports creation of multilingual layouts. Translate your website to any language with WPML!</li>
+</ul>
+<ul>
+<li><strong>Advanced typography options : </strong><br />Choose any of the Google web fonts library through powerful theme options panel! Now you can set a unique style for your brand!</li>
+</ul>
+<ul>
+<li><strong>Compatible with Contact form 7 : </strong><br />Metrobank is compatible with the most powerful and most popular custom contact forms WordPress widget! create your own forms in seconds!</li>
+</ul>
+<ul>
+<li><strong>Powerful framework : </strong><br />Metrobank is based on most popular, well established, powerful vafpress theme options framework!</li>
+</ul>
+<ul>
+<li><strong>Detailed documentation : </strong><br />Extensive documentation plus great video guides on how to setup and customize Metrobank will make your customisations super easy and fast!</li>
+</ul>
+<ul>
+<li><strong>Crossbrowser compatibility : </strong><br />Metrobank looks great among all major browsers including IE9+.</li>
+</ul>
+<h3 id="item-description__full-features-list">Full Features List</h3>
+<ul>
+<li>100% Fluid Responsive &ndash; Fits any device perfectly</li>
+<li>Tested on real devices</li>
+<li>Flexible Layout</li>
+<li>Use our demo layout or create your own visually different experience using page Metrobank and feature-rich backend.</li>
+<li>Unlimited Sidebars</li>
+<li>Retina Optimized</li>
+<li>Advanced Admin Panel</li>
+<li>Demo Import, Content and Sliders</li>
+<li>Social Links</li>
+<li>Bottom Footer Widgets</li>
+<li>Clean &amp; Commented Code</li>
+<li>Advanced Typography</li>
+<li>Google Fonts &ndash; 600+ Font families available</li>
+<li>Custom Font Support</li>
+<li>Custom Page Templates</li>
+<li>Pixel Perfect Design</li>
+<li>Quick &amp; Easy Installation &amp; Setup</li>
+<li>Custom CSS Ready</li>
+<li>HTML5 &amp; CSS3</li>
+<li>Easy Customization With Variable Content Sections</li>
+<li>Custom Build Theme &amp; Page Options</li>
+<li>SEO Ready</li>
+<li>Localizable with .pot translation files</li>
+<li>WPML Multilanguage Plugin Extended Support</li>
+<li>Various Post Formats</li>
+<li>Unlimited Pages</li>
+<li>Custom Widgets and smart admin panel for customizations</li>
+<li>Customizable Contact Forms with Contact Form 7 support</li>
+<li>FullScreen Background Slider</li>
+<li>Cross Browser Compatibility</li>
+<li>Well Documented &amp; More&hellip;.</li>
+</ul>
+
+
+agency, architecture, blog, business, corporate, creative, ecommerce, elementor, minimal, modern, multipurpose, photography, portfolio, dentist , doctor, freelancer
